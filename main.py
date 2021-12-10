2057fb95ee3f8fd12b05c07bb2570c914bcf4d02
19333690 <DSSenglyaev@sberbank.ru>
2021-12-10 18:22:44 +0300

    SWML-4091 add logs

--- main.py
+++ main.py
@@ -115,6 +115,7 @@ class ContextIncludedRoute(APIRoute):
                 result = DB.universal_select(select_done_req_with_response.format(request_id))
                 logger.info(f'got result {result}')
                 if result:
+                    logger.info(f'Come to result with {result}')
                     logger.info(f'{log_info} Got response. Status: {result[0].status}. Body: {result[0].response_body}')
                     body = {'message': result[0].status, 'response': result[0].response_body}
                     response.body = str(body).encode()
